8565f8eec9860954c1472e6c0ba7d8bff03988d4
tveten <tveten@nr.no>
2023-01-26 12:02:11 +0100

    Add illustration

--- example_LordenPollakCUSUM.py
+++ example_LordenPollakCUSUM.py
@@ -6,7 +6,7 @@ from streamchange.data import simulate
 
 df = simulate([0, 5], seg_lens=[100, 10])
 
-detector = LordenPollakCUSUM(4, 100)
+detector = LordenPollakCUSUM(rho=4, threshold=100)
 score = []
 res = []
 for t, (x, _) in enumerate(iter_pandas(df)):
@@ -15,6 +15,7 @@ for t, (x, _) in enumerate(iter_pandas(df)):
     if detector.change_detected:
         mean = detector.sum / detector.n
         res.append(dict(t=t, cpt=detector.changepoints, mean=mean))
-print(res)
 
-px.scatter(x=range(len(score)), y=score, render_mode="webgl")
+fig = px.scatter(x=range(len(score)), y=score, render_mode="webgl")
+fig.add_hline(detector.threshold, line_color="red")
+fig.show()
