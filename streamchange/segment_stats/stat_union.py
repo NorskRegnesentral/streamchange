4c01d92e2b335cd4ae2205556fec855332fb3829
Martin Tveten <matveten@gmail.com>
2023-02-12 21:09:07 +0100

    Add convenience functions to StatUnion

--- streamchange/segment_stats/stat_union.py
+++ streamchange/segment_stats/stat_union.py
@@ -2,16 +2,23 @@ import numbers
 import numpy as np
 import river
 from collections import deque
+from typing import Dict
 
 from .base import SegmentStat
 
 
 class StatUnion(SegmentStat):
-    def __init__(self, stats: dict, max_history = -np.inf):
+    def __init__(self, stats: Dict[str, SegmentStat], max_history=-np.inf):
         super().__init__(max_history)
         for stat in stats.values():
             stat.max_history = max_history
         self.stats = stats
+
+    def __getitem__(self, key):
+        return self.stats[key]
+
+    def __len__(self):
+        return len(self.stats)
 
     def items(self):
         return self.stats.items()
@@ -27,7 +34,7 @@ class StatUnion(SegmentStat):
             stat.reset()
         return self
 
-    def get(self, i=-1) -> dict:
+    def get(self, i=-1) -> Dict[str, float]:
         return {name: stat.get(i) for name, stat in self.items()}
 
     def update(self, x):
