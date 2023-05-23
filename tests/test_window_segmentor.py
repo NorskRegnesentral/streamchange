fe2bfcf8422b5bf5134acc389bfc526e3a204f80
tveten <tveten@nr.no>
2023-05-23 15:35:03 +0200

    Add tests for minsl, window sizes and changepoint candidates

--- tests/test_window_segmentor.py
+++ tests/test_window_segmentor.py
@@ -1,11 +1,13 @@
+import pytest
 from river.stream import iter_pandas
+import numpy as np
 
-from streamchange.amoc import CUSUM, WindowSegmentor
+from streamchange.amoc import CUSUM, CUSUM0, WindowSegmentor
 from streamchange.penalties import BIC
 from streamchange.data import simulate
 
 
-def test_accuracy():
+def test_sane_accuracy():
     seg_len = 100
     df = simulate([0, 30], [seg_len], p=1, seed=2)
     estimator = CUSUM(penalty=BIC(scale=10))
@@ -28,13 +30,29 @@ def test_varying_threshold():
         try:
             estimator = CUSUM(penalty=penalty)
             detector = WindowSegmentor(estimator, 2, 100)
-            for t, (x, _) in enumerate(iter_pandas(df)):
-                detector.update(x)
+            detector.fit(df)
         except Exception as exc:
             assert False, f"'detector.update()' raised exception {exc}"
 
 
-def test_detection_window_size():
+def test_window_sizes():
+    seg_len = 30
+    df = simulate([0, 10, 0, 10, 0], [seg_len], p=1, seed=5)
+    min_windows = [2, 10, 100]
+    max_windows = [2, 50, 500]
+    for min_window, max_window in zip(min_windows, max_windows):
+        try:
+            estimator = CUSUM()
+            detector = WindowSegmentor(estimator, min_window, max_window)
+            detector.fit(df)
+        except Exception as exc:
+            assert False, f"'detector.update()' raised exception {exc}"
+
+    with pytest.raises(Exception):
+        WindowSegmentor(CUSUM(), 10, 9)
+    with pytest.raises(Exception):
+        WindowSegmentor(CUSUM(), 1, 10)
+
     seg_len = 20
     df = simulate([0, 10, 0, 20, 0, 1, 0, 3, 5, 0, 4], [seg_len], p=1, seed=34)
     estimator = CUSUM(penalty=10)
@@ -48,3 +66,35 @@ def test_detection_window_size():
             most_recent_cpt = detector.changepoints[-1]
         else:
             most_recent_cpt += 1
+
+
+def test_minsl():
+    seg_len = 50
+    df = simulate([0, 10, 0], [seg_len], p=1, seed=5)
+    minsls = [1, 2, 10]
+    for minsl in minsls:
+        detector = WindowSegmentor(CUSUM(), minsl=minsl)
+        detector.fit(df)
+        cpts = np.array(detector.changepoints_)
+        assert np.all(np.diff(cpts) >= minsl)
+
+    with pytest.raises(Exception):
+        WindowSegmentor(CUSUM(), 2, 10, 6)
+    with pytest.raises(Exception):
+        WindowSegmentor(CUSUM0(), 2, 10, 11)
+
+
+def test_candidates():
+    seg_len = 50
+    df = simulate([0, 10, 0], [seg_len], p=1, seed=5)
+    try:
+        detector = WindowSegmentor(CUSUM(), candidate_type="linear", candidate_step=2)
+        detector.fit(df)
+        detector = WindowSegmentor(CUSUM(), candidate_type="linear", candidate_step=5)
+        detector.fit(df)
+        detector = WindowSegmentor(CUSUM(), candidate_type="geom", candidate_step=1.1)
+        detector.fit(df)
+        detector = WindowSegmentor(CUSUM(), candidate_type="geom", candidate_step=2.0)
+        detector.fit(df)
+    except Exception as exc:
+        assert False, f"'detector.fit()' raised exception {exc}"
