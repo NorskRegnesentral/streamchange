a88283a34c1d69e244e912749b9cc5fd1694096d
tveten <tveten@nr.no>
2023-05-24 16:38:43 +0200

    Use Penalty objects in LordenPollakCUSUM

--- interactive/example_LordenPollakCUSUM.py
+++ interactive/example_LordenPollakCUSUM.py
@@ -5,7 +5,7 @@ from streamchange.data import simulate
 
 series = simulate([0, 5], seg_lens=[100, 10])[0]
 
-detector = LordenPollakCUSUM(rho=4, threshold=100)
+detector = LordenPollakCUSUM(rho=4, penalty=100)
 score = []
 res = []
 for t, x in series.items():
@@ -16,5 +16,5 @@ for t, x in series.items():
         res.append(dict(t=t, cpt=detector.changepoints, mean=mean))
 
 fig = px.scatter(x=range(len(score)), y=score, render_mode="webgl")
-fig.add_hline(detector.threshold, line_color="red")
+fig.add_hline(detector.penalty(), line_color="red")
 fig.show()
--- streamchange/sequential/lorden_pollak.py
+++ streamchange/sequential/lorden_pollak.py
@@ -1,12 +1,16 @@
-import numbers
-
 from streamchange.base import ChangeDetector
+from numbers import Number
+from typing import Tuple
+
+from ..penalties import BasePenalty, ConstantPenalty
 
 
 class LordenPollakCUSUM(ChangeDetector):
-    def __init__(self, rho: numbers.Number, threshold: numbers.Number = 0):
+    def __init__(self, rho: Number, penalty: Tuple[BasePenalty, Number]):
+        if isinstance(penalty, Number):
+            penalty = ConstantPenalty(penalty)
+        self.penalty = penalty
         self.rho = rho
-        self.threshold = threshold
         self.reset()
 
     def reset(self):
@@ -15,13 +19,13 @@ class LordenPollakCUSUM(ChangeDetector):
         self.sum = 0.0
         self.score = 0.0
 
-    def update(self, x: numbers.Number):
+    def update(self, x: Number):
         super().reset()
 
         mean = self.sum / self.n if self.n > 0 else 0
         mu = max(mean, self.rho)
         self.score = max(0, self.score + mu * x - mu**2 / 2)
-        if self.score > self.threshold:
+        if self.score > self.penalty():
             self._changepoints = [self.n + 1]
 
         if self.score < 1e-8:
