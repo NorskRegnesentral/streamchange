b361597e37aeca71943efd6c37c49c5df8785893
tveten <tveten@nr.no>
2023-07-07 13:03:41 +0200

    Update PELT example

--- interactive/example_pelt.py
+++ interactive/example_pelt.py
@@ -3,7 +3,7 @@ import pandas as pd
 from streamchange.data import simulate
 from streamchange.pelt import Pelt, L2Cost
 
-df = simulate([0, 10, 0], [100000])
-detector = Pelt(L2Cost(), minsl=2, maxsl=100000)
+df = simulate([0, 10, 0, 10, 0], [1000])
+detector = Pelt(L2Cost(), minsl=2, maxsl=10000)
 segments = detector.fit_predict(df)
 print(pd.DataFrame(segments))
