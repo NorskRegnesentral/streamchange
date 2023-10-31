a7c007d06053a97cd38821cc6a2892c3e11d1feb
tveten <tveten@nr.no>
2023-10-31 15:35:54 +0100

    Add CUSUM0Score

---
+++ interactive/example_cusum0score.py
@@ -0,0 +1,47 @@
+import numpy as np
+import plotly.express as px
+from streamchange.data import simulate
+from streamchange.sequential.scores import CUSUM0Score
+from streamchange.sequential import (
+    AggregatedScore,
+    PenalisedScore,
+    SequentialChangeDetector,
+)
+
+# Univariate score
+x = simulate([0, 5, 0, 20], seg_lens=[100, 30, 100, 30])[0]
+score = CUSUM0Score(candidate_grid=[1, 2, 5, 10])
+score.fit(x)
+px.scatter(x)
+px.scatter(score.values_)
+
+x = simulate([0], seg_lens=[100000])[0]
+score = CUSUM0Score(candidate_grid=[10, 100, 1000, 10000])
+score.fit(x)
+
+
+# Multivariate score
+x = simulate([0, 5, 0, 20], seg_lens=[100, 30, 100, 30], p=5)
+candidate_grid = [2, 5, 10, 20]
+base_score = CUSUM0Score(candidate_grid)
+score = AggregatedScore(base_score, aggregator=np.sum)
+penalised_score = score.penalise(100)
+penalised_score.fit(x)
+px.scatter(penalised_score.values_)
+px.scatter(x.melt(ignore_index=False), color="variable", y="value")
+
+# Change detection
+x = simulate([0, -5, 0, 20], seg_lens=[100, 30, 100, 30], p=10)
+candidate_grid = [2, 5, 10, 20]
+base_score = CUSUM0Score(candidate_grid)
+score = AggregatedScore(base_score, aggregator=np.sum).penalise(100)
+detector = SequentialChangeDetector(score, reset_on_change=True, restart_delay=50)
+detector.fit(x)
+# fig = px.scatter(detector.penalised_scores_)
+fig = px.scatter(x.melt(ignore_index=False), color="variable", y="value")
+for alarm in detector.alarms_:
+    fig.add_vline(alarm, line_color="red")
+for cpt in detector.changepoints_:
+    fig.add_vline(cpt, line_color="blue")
+fig.show()
+px.scatter(detector.penalised_scores_)
--- streamchange/sequential/scores.py
+++ streamchange/sequential/scores.py
@@ -43,6 +43,11 @@ class BaseScore:
         self.values_ = pd.Series(self.values_, index=x.index)
         return self
 
+    @property
+    def changepoint(self):
+        """Optional property for those scores that also give esimates of the most recent changepoint location per iteration."""
+        return None
+
 
 class BaseRawScore(BaseScore):
     """A raw changepoint score. It is always >= 0"""
@@ -97,6 +102,10 @@ class PenalisedScore(BasePenalisedScore):
         self._penalised_score = self.score.value - self.penalty()
         return self
 
+    @property
+    def changepoint(self):
+        return self.score.changepoint
+
 
 class AggregatedScore(BaseRawScore):
     def __init__(self, base_score: BaseRawScore, aggregator: Callable = np.sum):
@@ -148,3 +157,162 @@ class LordenPollakScore(BaseRawScore):
             self.n += 1
             self.sum += x
         return self
+
+    @property
+    def changepoint(self):
+        return self.n + 1
+
+
+from river.stats import Sum
+from river.utils import Rolling
+
+
+class CUSUM0Score(BaseRawScore):
+    def __init__(self, candidate_grid: list):
+        self.candidate_grid = candidate_grid
+        self.weights = [1 / candidate for candidate in self.candidate_grid]
+        self.reset()
+
+    def reset(self):
+        self.sums = [
+            Rolling(Sum(), window_size=candidate) for candidate in self.candidate_grid
+        ]
+        super().reset()
+        return self
+
+    def update(self, x: Number):
+        for s in self.sums:
+            s.update(x)
+
+        self.cusum = [w * s.get() ** 2 for s, w in zip(self.sums, self.weights)]
+        self._score = max(self.cusum)
+
+    def changepoint(self):
+        return self.candidate_grid[np.argmax(self.cusum)]
+
+
+# from ..base import NumpyDeque
+# from ..utils import geomspace_int
+# from ..amoc import BaseAMOCEstimator
+
+
+# class AMOCScore(BasePenalisedScore):
+#     """
+#     Class for testing-based changepoint detection.
+
+#     Parameters
+#     ----------
+#     estimator
+#         The single changepoint test.
+#     min_window
+#         The minimum length of the window to test for changepoints in.
+#     max_window
+#         The maximum length of the window to test for changepoints in.
+#     minsl
+#         The minimum segment length.
+#     candidate_type
+#         The type of candidate changepoints set. Must be either "linear" or "geom".
+#     candidate_step
+#         The step size of the candidate changepoint set. If candidate_type=="geom",
+#         the step size is the factor to multiply the previous changepoint with to
+#         generate the set, and it must therefore be > 1.
+#     """
+
+#     def __init__(
+#         self,
+#         estimator: BaseAMOCEstimator,
+#         min_window: int = 2,
+#         max_window: int = int(1e5),
+#         minsl: int = 1,
+#         candidate_type: str = "linear",
+#         candidate_step: float = 1,
+#     ):
+#         self.estimator = estimator
+#         self._validate_window(min_window, max_window, minsl)
+#         self.min_window = min_window
+#         self.max_window = max_window
+#         self.minsl = minsl
+#         self.candidate_type = candidate_type
+#         self.candidate_step = candidate_step
+#         self.candidate_cpts = self._make_candidate_cpts()
+#         self.window = NumpyDeque(max_window)
+#         self.reset()
+
+#     def reset(self):
+#         super().reset()
+#         self.last_changepoint = 0
+#         self.estimator.reset()
+#         self.window.reset()
+#         return self
+
+#     def get_penalty(self):
+#         return self.estimator.penalty
+
+#     def _validate_window(self, min_window, max_window, minsl):
+#         if min_window < 2:
+#             raise ValueError("min_window cannot be smaller than 2.")
+#         if min_window > max_window:
+#             raise ValueError("min_window cannot be greater than max_window.")
+#         if minsl < max(self.estimator._minsl_before, self.estimator._minsl_after):
+#             msg = "minsl cannot be smaller than the strictest minsl restriction in the AMOC estimator."
+#             raise ValueError(msg)
+
+#         is_onesided_estimator = (
+#             self.estimator._minsl_after == 0 or self.estimator._minsl_before == 0
+#         )
+#         if is_onesided_estimator and minsl > max_window:
+#             msg = "minsl cannot be greater than max_window for one-sided AMOC estimators. "
+#             raise ValueError(msg)
+#         elif not is_onesided_estimator and minsl > max_window / 2:
+#             msg = "minsl cannot be greater than max_window/2 for two-sided AMOC estimators."
+#             raise ValueError(msg)
+
+#     def _make_candidate_cpts(self):
+#         # Candidate changepoints only run till n-1 to avoid the same changepoint
+#         # being tested twice, which could result in an infinite loop in .update()
+#         min_cpt = self.minsl if self.estimator._minsl_after > 0 else 0
+#         max_cpt = (
+#             self.max_window - self.minsl
+#             if self.estimator._minsl_before > 0
+#             else self.max_window
+#         )
+#         if max_cpt < min_cpt:
+#             message = "minsl cannot be greater than max_window/2 when the AMOC estimator requires estimation both before and after a candidate changepoint."
+#             raise ValueError(message)
+
+#         if self.candidate_type == "linear":
+#             candidate_cpts = np.arange(min_cpt, max_cpt + 1, self.candidate_step)
+#         elif self.candidate_type == "geom":
+#             if self.candidate_step <= 1.0:
+#                 message = "When candidate_type='geom', candidate_step must be > 1."
+#                 raise ValueError(message)
+#             candidate_cpts = geomspace_int(min_cpt, max_cpt + 1, self.candidate_step)
+#         else:
+#             raise ValueError("candidate_type must be either 'linear' or 'geom'.")
+
+#         return candidate_cpts
+
+#     def _get_valid_candidate_cpts(self):
+#         minsl_boundary = self.last_changepoint - self.minsl + 1
+#         valid_candidates = self.candidate_cpts < min(minsl_boundary, len(self.window))
+#         return self.candidate_cpts[valid_candidates]
+
+#     @property
+#     def changepoint(self):
+#         return self.estimator.changepoint
+
+#     def update(self, x):
+#         self.estimator.reset()
+#         self.window.appendleft(x)
+#         # Need some upper limit on the last changepoint index.
+#         self.last_changepoint = min(self.last_changepoint + 1, int(1e8))
+
+#         if len(self.window) >= self.min_window:
+#             candidate_cpts = self._get_valid_candidate_cpts()
+#             self.estimator.fit(self.window.values, candidate_cpts)
+#             self._penalised_score = self.estimator.score
+#             if self.estimator.change_detected:
+#                 self.last_changepoint = self.changepoint
+#                 self.window.pop(len(self.window) - self.changepoint)
+
+#         return self
